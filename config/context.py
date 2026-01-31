091de2d1636e66bb90a1d1b1caf4731fd769cfd5
David Dokupil <redder.surgery0e@icloud.com>
2026-01-31 13:26:31 -0600

    Fixed bug preventing templates from loading

--- config/context.py
+++ config/context.py
@@ -5,7 +5,7 @@ from .app_config import config
 __all__ = ['app', 'DB_CONFIG']
 
 _config_name = 'development'
-app = Flask(__name__)
+app = Flask('app')
 
 app.config.from_object(config.get(_config_name, config['default']))
 DB_CONFIG = app.config['DB_CONFIG']
