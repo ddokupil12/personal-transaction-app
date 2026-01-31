2b87e270bb4f0249a70fe0dee3fd843d7d3ecc43
David Dokupil <redder.surgery0e@icloud.com>
2026-01-30 18:35:25 -0600

    Simplified code

--- context.py
+++ context.py
@@ -6,5 +6,4 @@ config_name = 'development'
 app = Flask(__name__)
 
 app.config.from_object(config.get(config_name, config['default']))
-app.secret_key = app.config['SECRET_KEY']
 DB_CONFIG = app.config['DB_CONFIG']
