db59cd89d28d1099393c4f64a908a656ce96e5d4
David Dokupil <redder.surgery0e@icloud.com>
2026-02-05 21:24:28 -0600

    Removed some docstrings in compliance with PEP 8, PEP 257

--- app_config.py
+++ app_config.py
@@ -7,28 +7,18 @@ __all__ = ['config', 'is_dotenv_loaded']
 is_dotenv_loaded = load_dotenv()
 
 class _Config:
-    """
-    The default configuration for the app
-
-    Attributes:
-    SECRET_KEY: str (the server secret key)
-    DEBUG: bool (determines if debug mode is on)
-    PORT: int (the server's port)
-    DB_CONFIG: dict (the database settings, from environment variables)
-    """
     SECRET_KEY = environ.get('SECRET_KEY')
     DEBUG = True
-    PORT = environ.get('PORT')
+    PORT = environ.get('PORT') # The server's port
     DB_CONFIG = {
         'host': environ.get('DB_HOST'),
         'database': environ.get('DB_NAME'),
         'user': environ.get('DB_USER'),
         'password': environ.get('DB_PASSWORD'),
-        'port': int(environ.get('DB_PORT', 3306))
+        'port': int(environ.get('DB_PORT', 3306)) # The database's port
     }
 
 class _ProductionConfig(_Config):
-    """Production configuration"""
     DEBUG = False
 
 # _Configuration dictionary to allow selection of a configuration
