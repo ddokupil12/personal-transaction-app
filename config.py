99693babe457d225618c09804ac958505e4177ff
David Dokupil <redder.surgery0e@icloud.com>
2026-02-22 20:42:13 -0600

    Added comments

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 is_dotenv_loaded = load_dotenv()
 
-class _Config:
+class _Config: # Default app configuration
     SECRET_KEY = environ['SECRET_KEY']
     DEBUG = True
     PORT = environ.get('PORT') # The server's port
@@ -18,7 +18,7 @@ class _Config:
         'port': int(environ.get('DB_PORT', 3306)) # The database's port
     }
 
-class _ProductionConfig(_Config):
+class _ProductionConfig(_Config): # Production app configuration
     DEBUG = False
 
 # _Configuration dictionary to allow selection of a configuration
